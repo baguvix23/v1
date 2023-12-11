13d469d779e5991d444de9cb3869f0d1d99f95fd
baguvix23 <jordymazac@gmail.com>
2023-12-11 15:47:41 -0500

    Structured corrections in browsers

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -36,6 +36,18 @@ export default {
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
+      fontSize: {
+        12: "1.2rem", // 12px
+        14: "1.4rem", // 14px
+        16: "1.6rem", // 16px
+        18: "1.8rem", // 18px
+        20: "2.0rem", // 20px
+        24: "2.4rem", // 24px
+        32: "3.2rem", // 32px
+        48: "4.8rem", // 48px
+        56: "5.6rem", // 56px
+        64: "6.4rem", // 64px
+      },
     },
     extend: {
       backgroundImage: {
